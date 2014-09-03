f87bd483cbe74fa7c1f679cd5944bd4b66519877
sandeco <sandecom@gmail.com>
2014-09-03 15:15:34 -0300

    Reconfig Spring MVC

---
+++ application/macaxeira/src/main/java/com/macaxeira/controller/HelloWorldController.java
@@ -0,0 +1,15 @@
+package com.macaxeira.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HelloWorldController {
+	
+	@RequestMapping(value="/Teste")
+	public @ResponseBody String criarTeste(){
+		return "teste";
+	}
+
+}
