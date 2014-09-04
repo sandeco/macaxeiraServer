3acfa1bc037ce4498965882ae9d8e1d40e3850aa
sandeco <sandecom@gmail.com>
2014-09-04 15:54:42 -0300

    Gerando primeiras entidades do banco de dados

---
+++ application/src/com/macaxeira/model/Produto.java
@@ -0,0 +1,53 @@
+package com.macaxeira.model;
+
+import java.io.Serializable;
+import javax.persistence.*;
+
+
+/**
+ * The persistent class for the produto database table.
+ * 
+ */
+@Entity
+@NamedQuery(name="Produto.findAll", query="SELECT p FROM Produto p")
+public class Produto implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	private int id;
+
+	private String nome;
+
+	//bi-directional many-to-one association to Categoria
+	@ManyToOne
+	private Categoria categoria;
+
+	public Produto() {
+	}
+
+	public int getId() {
+		return this.id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return this.nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public Categoria getCategoria() {
+		return this.categoria;
+	}
+
+	public void setCategoria(Categoria categoria) {
+		this.categoria = categoria;
+	}
+
+}
