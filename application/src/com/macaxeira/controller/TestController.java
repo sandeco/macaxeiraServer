ee18bbc405b56b51e4599e0aa1a2147203ac00e2
sandeco <sandecom@gmail.com>
2014-09-04 09:56:26 -0300

    resolução spring mvc

--- application/src/com/macaxeira/controller/TestController.java
+++ application/src/com/macaxeira/controller/TestController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
-public class HelloWorldController {
+public class TestController {
 	
 	@RequestMapping(value="/Teste")
 	public @ResponseBody String criarTeste(){
